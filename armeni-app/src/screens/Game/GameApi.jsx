a2620f17b4cdccf781c76765089dbddcadb92325
Orin Israely <orin.israely@gmail.com>
2022-06-10 20:07:05 +0300

    Game poc with mock data

--- armeni-app/src/screens/Game/GameApi.jsx
+++ armeni-app/src/screens/Game/GameApi.jsx
@@ -2,13 +2,28 @@ import React from 'react';
 import {useAxiosGet} from '../../network/Api';
 import Game from '../../components/Game'
 
+const mockData = {
+    currentPlayer : {
+        name: 'Orin',
+        cards: ['A', 'B', 'C']
+    },
+    otherPlayers : [
+        {name:'Iddo',
+        numberOfCards: 5},
+        {name:'Jony',
+        numberOfCards: 5}
+    ],
+    cardsInDeck : 25,
+    topCardInPile : ['Ace']
+  }
+
 // This functionall component that wraps the armeni game and is responsible to get all data from server for initializing the game
 const GameApi = () => {
     const { data, error, isLoading } = useAxiosGet('/test_api');
     if(isLoading) return (<h1>Loading....</h1>);
     if(error) return (<p>{error}</p>);
     return(
-    <Game data={data ?? []}/>
+        <Game data={mockData ?? []}/>
     )
 };
 export default GameApi;
